27ba146cfbb1ffb202c650cccd029ffe610ceaa8
GregLebreton <49302129+GregLebreton@users.noreply.github.com>
2019-04-22 17:14:12 +0200

    Update scriptjenkins.sh

--- scriptjenkins.sh
+++ scriptjenkins.sh
@@ -8,7 +8,7 @@
 
 # récupération de la version désirée passée à jenkins (snapshot, release)
 
-version_artifact=1.10-SNAPSHOT
+version_artifact=$1
 
 # définition des variables d'environnement nécessaires
 
